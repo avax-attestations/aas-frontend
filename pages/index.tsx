1fe84aed1e04808dc4228e259b58a4170956f25d
Borg <brussel17891@gmail.com>
2024-03-02 08:32:52 -0300

    Redirect to schemas page from home
    Signed-off-by: Borg <brussel17891@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function HomeNextPage() {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    push('/schemas');
+  }, [push])
+
   return (
     <h1 className="text-3xl font-bold">Home</h1>
   );
