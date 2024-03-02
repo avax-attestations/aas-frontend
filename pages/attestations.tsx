6197f1bb45c9844519b7882596891cf17241ddca
Borg <brussel17891@gmail.com>
2024-03-02 08:35:28 -0300

    Redirect from attestations to schemas
    Page is not implemented
    
    Signed-off-by: Borg <brussel17891@gmail.com>

--- pages/attestations.tsx
+++ pages/attestations.tsx
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function AttestationsPage() {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    push('/schemas');
+  }, [push])
+
   return (
     <h1 className="text-3xl font-bold">Attestations</h1>
   );
