e56fe8f1cf4dcaf92ed7488869309cdb7798b15b
Borg <brussel17891@gmail.com>
2024-03-16 11:09:12 -0300

    Reduce Sepolia batch
    Signed-off-by: Borg <brussel17891@gmail.com>

--- lib/config.ts
+++ lib/config.ts
@@ -83,7 +83,7 @@ export const DEPLOYMENT = {
       deploymentTxn: sepoliaEAS.transactionHash as Hash,
       abi: sepoliaEAS.abi as Abi
     },
-    blockBatchSize: 10n,
+    blockBatchSize: 2n,
     delayBetweenRPCRequests: 1000,
     transportFactory: () => {
       const apiKey = process.env.SEPOLIA_ALCHEMY_API_KEY;
