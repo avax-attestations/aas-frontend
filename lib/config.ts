b7f9aac64e96f8b00fc0147274b852f0b2155b7f
Borg <brussel17891@gmail.com>
2024-03-16 11:01:04 -0300

    Restore arbitrum batch
    Signed-off-by: Borg <brussel17891@gmail.com>

--- lib/config.ts
+++ lib/config.ts
@@ -61,7 +61,7 @@ export const DEPLOYMENT = {
       deploymentTxn: arbitrumEAS.transactionHash as Hash,
       abi: arbitrumEAS.abi as Abi
     },
-    blockBatchSize: 100n,
+    blockBatchSize: 25000n,
     delayBetweenRPCRequests: 0,
     transportFactory: () => {
       return http(undefined, {
