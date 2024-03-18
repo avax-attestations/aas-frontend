6addd0ac5e382a9134855700e6a3fa430348d4c3
Borg <brussel17891@gmail.com>
2024-03-18 20:50:32 -0300

    Increase resume script kill timeout
    Signed-off-by: Borg <brussel17891@gmail.com>

--- scripts/resume-indexing.ts
+++ scripts/resume-indexing.ts
@@ -71,7 +71,7 @@ async function runChain(chain: Chain) {
     stdio: 'inherit'
   })
 
-  const timeoutSeconds = 50 * 60  // kill after 50 minutes
+  const timeoutSeconds = 2 * 60 * 60  // kill after 2 hours
   setTimeout(() => {
     console.log(`Killing indexing process for "${chain}" after ${timeoutSeconds} seconds`)
     indexingProcess.kill('SIGUSR1')
