958ca8bf29edc4574765ce76370868aa06920775
Borg <brussel17891@gmail.com>
2024-03-18 10:34:06 -0300

    Add button to clear search
    Signed-off-by: Borg <brussel17891@gmail.com>

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { type ReadonlyURLSearchParams } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { X } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 export interface SearchFormProps {
@@ -15,7 +16,8 @@ export function SearchForm({
 }: SearchFormProps) {
   const searchString = searchParams.get('search') ?? '';
   const [search, setSearch] = useState('');
-  const ref = useRef(null);
+  const searchRef = useRef(null);
+  const clearRef = useRef(null);
 
   useEffect(() => {
     setSearch(searchString);
@@ -28,6 +30,7 @@ export function SearchForm({
     } else {
       params.set('search', search);
     }
+    params.delete('page');
     return {
       search: params.toString()
     }
@@ -40,17 +43,24 @@ export function SearchForm({
         name="search"
         value={search}
         onKeyUp={(e) => {
-          if (e.key === 'Enter' && ref.current !== null) {
-            (ref.current as any).click();
+          if (e.key === 'Enter' && searchRef.current !== null) {
+            (searchRef.current as any).click();
+          } else if (e.key === 'Escape' && clearRef.current !== null) {
+            (clearRef.current as any).click();
           }
         }}
         onChange={(e) => setSearch(e.target.value)}
         placeholder={placeholder} />
       <Button variant="secondary" asChild>
-        <Link ref={ref} href={getSearchUrl(search)}>
+        <Link ref={searchRef} href={getSearchUrl(search)}>
           Search
         </Link>
       </Button>
+      {searchString && (
+        <Link ref={clearRef} href={getSearchUrl('')}>
+          <X />
+        </Link>
+      )}
     </div>
   )
 }
