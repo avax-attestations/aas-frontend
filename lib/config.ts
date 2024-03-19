bb93bb185d6b7fa89105fa04907f2392578a5d58
Borg <brussel17891@gmail.com>
2024-03-19 06:10:03 -0300

    Set optimism batch size to 256
    Signed-off-by: Borg <brussel17891@gmail.com>

--- lib/config.ts
+++ lib/config.ts
@@ -87,8 +87,8 @@ export const DEPLOYMENT = {
       deploymentTxn: '0xffee154ed11ab8df29739104900f22450bec3e85790c01a632f04b0056cb92fd' as Hash,
       abi: optimismEAS.abi as Abi
     },
-    blockBatchSize: 128n,
-    delayBetweenRPCRequests: 0,
+    blockBatchSize: 256n,
+    delayBetweenRPCRequests: 100,
     transportFactory: () => {
       return http(undefined, {
         batch: {
